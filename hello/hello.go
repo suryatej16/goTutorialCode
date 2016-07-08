a2abc2986f702b98776ca095e43d9c83b7d82c82
Suryatej Mukkamalla <suryatej.mukkamalla@zuora.com>
2016-07-08 14:41:55 -0700

    Initial commit of new structure.

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/suryatej16/stringutil"
+)
+
+func main() {
+	fmt.Printf("Testing hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG , olleH"))
+}
